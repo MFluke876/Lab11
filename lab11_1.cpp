80e318258db02668ea7c592ab9f10ab7629b5ac8
Fluke <dr.catth@gmail.com>
2024-01-01 16:19:15 +0700

    lab11_1.cpp completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,16 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main(){
+    string grade[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+
+    srand(time(0));
+
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    cout << "You will get " << grade[rand()%8] << " in this 261102.";
+}
